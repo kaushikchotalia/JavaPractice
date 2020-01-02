2ef225fd52e5848ea1002a526defeebdb27128cb
Kaushik <chotalia.kaushik@gmail.com>
2020-01-01 21:09:42 -0500

    second commit

---
+++ src/Circle1.java
@@ -0,0 +1,27 @@
+public class Circle1 {
+    private double x,y;
+    private double r;
+    double circumference()
+    {
+        return 2 * 3.14* r ;
+    }
+    double area()
+    {
+        return  3.14* r * r;
+    }
+
+    /*// set r with this
+    public void setRadius(double r)
+    {
+        this.r = r;
+    }*/
+    /*//declare argument name different for setter method
+    public void setRadius(double p)
+    {
+        r = p;
+    }*/
+
+    public Circle1(double r) {
+        this.r = r;
+    }
+}
---
+++ src/StaticAndNonStatic.java
@@ -0,0 +1,29 @@
+public class StaticAndNonStatic {
+    static int i = 100;
+    static String s = "Beginnersbook";
+    //Static method
+    static void display()
+    {
+        System.out.println("i:"+i);
+        System.out.println("i:"+s);
+    }
+    //non-static method
+    void funcn()
+    {
+        //Static method called in non-static method
+        //display();
+        System.out.println("i:"+i);
+        System.out.println("i:"+s);
+    }
+    //static method
+    public static void main(String args[])
+    {
+        StaticAndNonStatic obj = new StaticAndNonStatic();
+        //You need to have object to call this non-static method
+        obj.funcn();
+
+        //Static method called in another static method
+        display();
+    }
+
+}
