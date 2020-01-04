ffbfe4865bb74f1e43a8c417b361d18c1df2ba09
Kaushik <chotalia.kaushik@gmail.com>
2020-01-03 20:53:43 -0500

    4th Commit

---
+++ src/DisplayMultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DisplayMultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter number for that multiplication table is required");
+        int n = sc.nextInt();
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(n+" X "+i+" = "+n*i);
+        }
+
+    }
+
+
+}
--- src/RightAngleTriangle.java
+++ src/RightAngleTriangle.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class RightAngleTriangle {
     public static void main(String[] args) {
-        int n = 5;
-        int num = 1;
-        for (int i = 0; i < n; i++) {
-            num = 1;
-            for (int j = 0; j <= i; j++)
+        int n = 10;
+        //not printing in triangle manner
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 1; j <= i; j++)
             {
-                System.out.println(num++);
-
+                System.out.println(j);
             }
+
             System.out.println("");
+
         }
+
     }
 }
 
